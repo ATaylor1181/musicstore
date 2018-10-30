7649dd63565cebb0706565916bbcc58f4dc6d6f8
Ross Savill <rosssavill@Rosss-MacBook-Air.local>
2018-10-30 15:46:04 +1100

    Decimal now appears as currency, stripe pulls across correct figure.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -71,6 +71,8 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:title, :description, :price, :image)
+      result = params.require(:product).permit(:title, :description, :price, :image)
+      result[:price] = result[:price].to_f * 100.0
+      result
     end
 end
