15aa20d67279399dda7dcd1c04378f28466ace47
Yong Liang <yongl9460@gmail.com>
2018-10-31 16:19:31 +1100

    Added Category and Subcategory models, and seeded the data in both models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_30_050935) do
+ActiveRecord::Schema.define(version: 2018_10_31_051125) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,12 @@ ActiveRecord::Schema.define(version: 2018_10_30_050935) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.string "family"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -48,6 +54,14 @@ ActiveRecord::Schema.define(version: 2018_10_30_050935) do
     t.string "city"
     t.string "state"
     t.index ["user_id"], name: "index_products_on_user_id"
+  end
+
+  create_table "subcategories", force: :cascade do |t|
+    t.string "instrument"
+    t.bigint "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_subcategories_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -65,4 +79,5 @@ ActiveRecord::Schema.define(version: 2018_10_30_050935) do
   end
 
   add_foreign_key "products", "users"
+  add_foreign_key "subcategories", "categories"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,63 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.delete_all
+Subcategory.delete_all
+
+
+string_instrument = Category.create( family: "String")
+Subcategory.create(instrument: "Banjo", category_id: string_instrument.id)
+Subcategory.create(instrument: "Cello", category_id: string_instrument.id)
+Subcategory.create(instrument: "Cittern", category_id: string_instrument.id)
+Subcategory.create(instrument: "Guitar", category_id: string_instrument.id)
+Subcategory.create(instrument: "Harp", category_id: string_instrument.id)
+Subcategory.create(instrument: "Lute", category_id: string_instrument.id)
+Subcategory.create(instrument: "Upright Bass", category_id: string_instrument.id)
+Subcategory.create(instrument: "Viola", category_id: string_instrument.id)
+Subcategory.create(instrument: "Violin", category_id: string_instrument.id)
+
+
+wood_wind = Category.create( family: "Woodwind")
+Subcategory.create(instrument: "Clarinet", category_id: wood_wind.id)
+Subcategory.create(instrument: "Flute", category_id: wood_wind.id)
+Subcategory.create(instrument: "Oboe", category_id: wood_wind.id)
+Subcategory.create(instrument: "Pipe", category_id: wood_wind.id)
+Subcategory.create(instrument: "Recorder", category_id: wood_wind.id)
+Subcategory.create(instrument: "Reeds", category_id: wood_wind.id)
+Subcategory.create(instrument: "Saxophone", category_id: wood_wind.id)
+
+
+percussion = Category.create( family: "Percussion")
+Subcategory.create(instrument: "Acoustic Drum Set", category_id: percussion.id)
+Subcategory.create(instrument: "Bells", category_id: percussion.id)
+Subcategory.create(instrument: "Cymbals", category_id: percussion.id)
+Subcategory.create(instrument: "Castanets", category_id: percussion.id)
+Subcategory.create(instrument: "Congas", category_id: percussion.id)
+Subcategory.create(instrument: "Electric Drum Set", category_id: percussion.id)
+Subcategory.create(instrument: "Frame Drums", category_id: percussion.id)
+Subcategory.create(instrument: "Hand Drums", category_id: percussion.id)
+Subcategory.create(instrument: "Maracas", category_id: percussion.id)
+Subcategory.create(instrument: "Xylophone", category_id: percussion.id)
+
+
+
+brass = Category.create( family: "Brass")
+Subcategory.create(instrument: "Bugle", category_id: brass.id)
+Subcategory.create(instrument: "Cornet", category_id: brass.id)
+Subcategory.create(instrument: "Euphonium", category_id: brass.id)
+Subcategory.create(instrument: "Flugelhorn", category_id: brass.id)
+Subcategory.create(instrument: "Helicon", category_id: brass.id)
+Subcategory.create(instrument: "Horn", category_id: brass.id)
+Subcategory.create(instrument: "Mellophone", category_id: brass.id)
+Subcategory.create(instrument: "Trombone", category_id: brass.id)
+Subcategory.create(instrument: "Trumpet", category_id: brass.id)
+Subcategory.create(instrument: "Tuba", category_id: brass.id)
+
+
+keyboard = Category.create( family: "Keyboard")
+Subcategory.create(instrument: "Accordions & Concertinas", category_id: keyboard.id)
+Subcategory.create(instrument: "Harpsichord", category_id: keyboard.id)
+Subcategory.create(instrument: "Keyboard", category_id: keyboard.id)
+Subcategory.create(instrument: "Piano", category_id: keyboard.id)
+Subcategory.create(instrument: "Sound Synthesizers", category_id: keyboard.id)
