c4c406b17953ab6f08ac4533e5adc53e865ceb78
Ross Savill <rosssavill@Rosss-MacBook-Air.local>
2018-11-01 14:11:56 +1100

    added product param - email syntax fixed

--- app/mailers/product_mailer.rb
+++ app/mailers/product_mailer.rb
@@ -1,6 +1,7 @@
 class ProductMailer < ApplicationMailer
     def new_purchase
         @user = params[:user]
+        @product = params[:product]
         mail(to: @user.email, subject: 'Thank you for your purchase!')
       end
 end
