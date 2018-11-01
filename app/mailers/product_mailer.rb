a542a361342b07257357c1763c6741f73f336bfc
Ross Savill <rosssavill@Rosss-MacBook-Air.local>
2018-11-01 13:51:41 +1100

    mid-mailer fix

--- app/mailers/product_mailer.rb
+++ app/mailers/product_mailer.rb
@@ -1,5 +1,5 @@
 class ProductMailer < ApplicationMailer
-    def welcome_email
+    def new_purchase
         @user = params[:user]
         mail(to: @user.email, subject: 'Thank you for your purchase!')
       end
