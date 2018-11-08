36ff513a68b3f29fe2915b0b08c2c0b03e04db53
Taylor <alex_taylor09@hotmail.com>
2018-11-09 10:33:28 +1100

    heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
- Subcategory.delete_all
- Category.delete_all
+ #Subcategory.delete_all
+ #Category.delete_all
 
 
 
